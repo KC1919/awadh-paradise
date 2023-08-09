58d40f91a5f90c450da8e9cfadf8e49b5f8593de
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-08-09 18:31:34 +0530

    added services section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import CustomNavbar from "./components/CustomNavbar";
 import Slider from "./components/Slider";
+import Service from "./components/Service";
 
 function App() {
   return (
     <div>
       <CustomNavbar />
       <Slider />
+      <Service/>
     </div>
   );
 }
